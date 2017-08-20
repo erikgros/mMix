58c4098b49c1cf8f26ad0e6bab3bb66099b7d2d7
Erik <erik.gros@rwth-aachen.de>
2017-08-20 17:01:43 +0200

    Improved YoungLaplace

--- YoungLaplace/dxztds.m
+++ YoungLaplace/dxztds.m
@@ -1,10 +1,12 @@
 function der = dxztds(xzt, s)
 % xzt = (x, z, theta)
+bot = -1; % -1/1 top/bottom meniscus (Bretherton)
+ R = 3; % result very insensitive to this value
  der(1) = cos( xzt(3) ); %dxds
  der(2) = sin( xzt(3) ); %dzds
  if (s == 0)
-  der(3) = -1;
+  der(3) = -1 / R;
  else
-  der(3) = -xzt(2) -sin( xzt(3) ) / xzt(1);
+  der(3) = bot*xzt(2) -sin( xzt(3) ) / xzt(1);
  end
 end
--- YoungLaplace/pendant.m
+++ YoungLaplace/pendant.m
@@ -1,49 +1,48 @@
-% The shape of a pendant drop/bubble is governed by:
-% B z + z" / (1 + z'^2)^3/2 + ax * z' r^-1 / (1 + z'^2)^1/2 = 0
-% see also: "https://link.springer.com/book/10.1007%2F978-1-4613-8584-4"
-ax=1;
-N=5001; % points
+% Shape of a pendant drop and top | bottom of rising bubble as described by:
+%   (+|-)z / Lc^2 + z" / (1 + z'^2)^3/2 + z' r^-1 / (1 + z'^2)^1/2 = 0
+% Using dimensionless form, lengths are scaled by Lc = sqrt(sig / (rho*g)).
+% The physical parameters only determine the scaling via Lc.
+% The tip of drop has radius R and elevation z0 = 2 / R.
+% Also interesting: "https://link.springer.com/book/10.1007%2F978-1-4613-8584-4"
+N = 500; % number of points
 
-rho=1;
-g=9.81;
-sig=20;
-Lc = sqrt(sig / (rho*g));
-
+function [h dhdr dydt2] = zr(R, z0, t)
 %%% primitive method %%%
-R = Lc; % tip radius
-B = Lc^(-2)
-t = linspace (0, R, N); % radius
-z = ones(N,2); dzdr = zeros(N,2);
-y = z(1) * ones(2,1); % y(1) = z; y(2) = z'
-dt = t(2) - t(1)
-for i=1:N-1
- if (i==1)
-  y(2) = -dt * 1 / R;
- else
-  dydt1 = y(2);
-  dydt2 = -ax * (1 + y(2).^2) .* y(2) ./ t(i) - B * y(1) .* (1 + y(2).^2)^(3/2);
-  y(1) += dt * dydt1;
-  y(2) += dt * dydt2;
+ h = z0 * ones(length(t),1);
+ y = z0 * ones(2,1); % y(1) = z; y(2) = z'
+ dt = t(2) - t(1);
+ for i=1:length(t)-1
+  if (i==1)
+   y(2) = -dt * 1 / R;
+  else
+   dydt1 = y(2);
+   dydt2 = -(1 + y(2).^2) .* y(2) ./ t(i) - y(1) .* (1 + y(2).^2)^(3/2);
+%   dydt2 = - y(1) .* (1 + y(2).^2)^(3/2); % (2d)
+   y(1) += dt * dydt1;
+   y(2) += dt * dydt2;
+  end
+  h(i+1) = y(1);
+  dhdr(i+1) = y(2);
  end
- z(i+1) = y(1);
- dzdr(i+1) = y(2);
 end
 %%%%%%%%%%%%%%%%%%%%%%%%
 
-plot( t, z );hold on; xlabel('r'); ylabel('z')
-if (ax == 0)
-% comparing with analytical solution for 2d:
- drdz = tan( asin(B*t.*t/2 - 1));
- figure(2)
- plot( t, dzdr, t, 1./drdz, 'g' )
- legend('numerical','analytical')
-else
-% comparing with xzt formulation:
- x0 = 0;
- z0 = z(1) / Lc;
- s = t / Lc;
- a = lsode( "dxztds", [x0 z0 0], s);
- plot( Lc*a(:,1), Lc*a(:,2),'r' )
- legend('primitive','advanced')
+col = ['r','b','g','y'];
+
+R = 1.15 * sqrt(0.842); % tip radius
+for k = 1:4
+ z0 = 2 / R;
+% r = linspace (0, R, N); % radius
+% [z dzdr d2zdr2I] = zr(R, z0, r);
+% plot( r(1:10:end), z(1:10:end), [col(k) "+"] );hold on;
+% using xzt formulation (requires max(s) = pi*R):
+ s = linspace (0, pi*R, N); % arc length
+ a = lsode( "dxztds", [0 z0 0], s);
+ plot( a(1:1:end,1), a(1:1:end,2), [col(k) "-"] );hold on;
+ R *= 1/1.15;
 end
-%plot(t, sqrt(R*R - t.*t), 'bk') % circle
+xlabel('r'); ylabel('z')
+return
+% comparing with analytical solution for 2d:
+ drdz = -tan( asin((z.*z-z0*z0)/2 + 1));
+ figure(2); plot( r, dzdr, 'r*', r, 1./drdz); legend('numerical','analytical')
